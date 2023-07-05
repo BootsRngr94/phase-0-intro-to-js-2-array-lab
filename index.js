5fc081aa6d41de21bcc6228179df7f98a0858cd6
Luis Westberry <takuame7@gmail.com>
2023-07-05 17:15:58 -0500

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = [
+    'Milo', 
+    'Otis', 
+    'Garfield'
+];
+function destructivelyAppendCat(name){
+    cats.push('Ralph');
+}
+function destructivelyPrependCat(name){
+    cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat(){
+    cats.pop('Garfield');
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift('Milo');
+}
+function appendCat(name){
+    const newArray = cats.slice();
+    newArray.push(name)
+    return newArray
+}
+function prependCat(name){
+    const newArray = cats.slice();
+    newArray.unshift(name)
+    return newArray
+}
+function removeLastCat(){
+    const newArray = cats.slice();
+    newArray.pop('Garfield')
+    return newArray
+}
+function removeFirstCat(){
+    const newArray = cats.slice();
+    newArray.shift('Milo')
+    return newArray
+}
